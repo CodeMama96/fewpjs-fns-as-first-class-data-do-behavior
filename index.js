f3c5545b8165ad6c482af92271f70759ed8af0cd
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-27 13:54:05 -0500

    commit tree 1c9a95987476c5a4d1d5cc213a03ed38cc3985fe

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+  // if (time< 12:00) //parseInt(12, 100) ?
+  // return "Good Morning"
+  // else (time is between 12:00-27:00)
+  // return "Good Afternoon"
+  // else (time > 17:00)
+  // return "Good Evening"
+}
+
+function displayMessage(greet){
+  document.getElementById("greeting").innerText = greet;
+}
